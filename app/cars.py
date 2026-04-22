29b2a4ab577e654676fe7743f4edfeb5914d6f8c
ArturObara <91539968+ArturObara@users.noreply.github.com>
2026-04-22 14:54:07 +0200

    feat: connect cars endpoints to SQLite with SQLAlchemy

--- app/cars.py
+++ app/cars.py
@@ -1,58 +1,60 @@
 from fastapi import FastAPI, HTTPException
 from app.schemas import CarCreate,CarResponse
-
-cars = [
-    {
-    "id" : 1, 
-    "brand": "Audi",
-    "model": "A5",
-    "year": 2020,
-    "available": True
-    },
-    {
-    "id" : 2, 
-    "brand": "BMW",
-    "model": "M2",
-    "year": 2023,
-    "available": False
-    },
-    {
-    "id" : 3, 
-    "brand": "Mercedes",
-    "model": "CLA",
-    "year": 2018,
-    "available": True
-    },
-    {
-    "id" : 4, 
-    "brand": "Audi",
-    "model": "A4",
-    "year": 2024,
-    "available": False
-    }
-]
+from app.database import SessionLocal
+from app.models import Car
 
 def register_car_routes(app: FastAPI):
+    
+    @app.get("/")
+    def root():
+        return {"message": "API is running"}
+
+
     @app.get("/cars")
     def cars_root():
+        db = SessionLocal()
+        cars = db.query(Car).all()
+        db.close()
         return cars
-
+    
     @app.get("/cars/{car_id}", response_model=CarResponse)
     def get_car_root(car_id: int):
-        for car in cars:
-            if car["id"] == car_id:
-                return car 
+        db = SessionLocal()
+        car = db.query(Car).filter(Car.id == car_id).first()
+        db.close()
+
+        if car:
+            return car
         raise HTTPException(status_code=404, detail="Car not found")
-        
+    
     @app.post("/cars")
     def add_car_root(car: CarCreate):
-        cars.append(car.model_dump())
+        db = SessionLocal()
+
+        new_car = Car(
+            id=car.id,
+            brand=car.brand,
+            model=car.model,
+            year=car.year,
+            available=car.available
+        )
+
+        db.add(new_car)
+        db.commit()
+        db.close()
+
         return {"message": f"New car {car.brand}: {car.model} added"}
 
     @app.delete("/cars/{car_id}")
     def delete_car_root(car_id: int):
-        for car in cars:
-            if car["id"] == car_id:
-                cars.remove(car)
-                return {"message": "Car deleted"}
+        db = SessionLocal()
+        car = db.query(Car).filter(Car.id == car_id).first()
+
+        if car:
+            db.delete(car)
+            db.commit()
+            db.close()
+            return {"message": "Car deleted"}
+
+        db.close()
         raise HTTPException(status_code=404, detail="Car not found")
