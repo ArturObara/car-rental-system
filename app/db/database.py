c2fe014f89f54c15c0669e3126a4ed1ac7284901
ArturObara <91539968+ArturObara@users.noreply.github.com>
2026-05-11 18:13:44 +0200

    chore(docker): containerize the application

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost/car_rental"
+DATABASE_URL = "postgresql+psycopg://postgres:admin123@host.docker.internal:5432/car_rental"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
