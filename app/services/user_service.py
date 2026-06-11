9f395e066f82895c4207cd9f302af3a142fe96cd
ArturObara <91539968+ArturObara@users.noreply.github.com>
2026-06-11 12:58:34 +0200

    style: clean up imports, indentation and add type hints in user service

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,21 +1,19 @@
 from sqlalchemy.orm import Session
 from app.db.models import User
 from app.schemas.schemas import UserCreate, UserLogin
-from app.config.security import hash_password
-from app.config.security import  verify_password
+from app.config.security import hash_password, verify_password
 
 
-def create_user(db: Session, user_data: UserCreate):
+def create_user(db: Session, user_data: UserCreate) -> User | None:
     existing_user = db.query(User).filter(User.email == user_data.email).first()
 
     if existing_user:
         return None
     
-    
     new_user = User(
-    name=user_data.name,
-    email=user_data.email,
-    hashed_password=hash_password(user_data.password)
+        name=user_data.name,
+        email=user_data.email,
+        hashed_password=hash_password(user_data.password)
     )
 
     db.add(new_user)
@@ -23,12 +21,14 @@ def create_user(db: Session, user_data: UserCreate):
     db.refresh(new_user)
     return new_user
 
-def authenticate_user(db: Session, login_data: UserLogin):
+
+def authenticate_user(db: Session, login_data: UserLogin) -> User | None:
     existing_user = db.query(User).filter(User.email == login_data.email).first()
 
     if not existing_user:
         return None
+        
     if not verify_password(login_data.password, existing_user.hashed_password):
-            return None
+        return None
     
     return existing_user
