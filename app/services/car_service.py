6335431cccda745a82670e9c84a88202c72c2867
ArturObara <91539968+ArturObara@users.noreply.github.com>
2026-06-11 12:47:55 +0200

    refactor: optimize car service with Pydantic unpacking and DRY

--- app/services/car_service.py
+++ app/services/car_service.py
@@ -2,37 +2,30 @@ from sqlalchemy.orm import Session
 from app.db.models import Car
 from app.schemas.schemas import CarCreate
 
-def get_all_cars(db: Session):
+def get_all_cars(db: Session) -> list[Car]:
     return db.query(Car).all()
 
-def get_car_by_id(db: Session, car_id: int):
+
+def get_car_by_id(db: Session, car_id: int) -> Car | None:
     return db.query(Car).filter(Car.id == car_id).first()
 
-def create_car(db: Session, car_data: CarCreate):
-    existing_car = db.query(Car).filter(Car.id == car_data.id).first()
-    if existing_car:
-        return None
+
+def create_car(db: Session, car_data: CarCreate) -> Car:
+    new_car = Car(**car_data.model_dump())
     
-    new_car = Car(
-        id=car_data.id,
-        brand=car_data.brand,
-        model=car_data.model,
-        year=car_data.year,
-        available=car_data.available
-    )
     db.add(new_car)
     db.commit()
     db.refresh(new_car)
 
     return new_car
 
-def delete_car(db: Session, car_id: int):
-    car = db.query(Car).filter(Car.id == car_id).first()
+
+def delete_car(db: Session, car_id: int) -> bool:
+    car = get_car_by_id(db, car_id)
 
     if not car:
-        return None
+        return False
     
     db.delete(car)
     db.commit()
     return True
-    
--- app/services/rentals.py
+++ app/services/rentals.py
@@ -3,7 +3,6 @@ from app.db import models
 from datetime import date, timedelta
 
 def create_new_rental(db: Session, car_id: int, user_id: int, days: int = 7):
-    
     car = db.query(models.Car).filter(models.Car.id == car_id).with_for_update().first()
     
     if not car or not car.available:
