7c67df6e4203615d5c3e04d94122cc72e456e46b
ArturObara <91539968+ArturObara@users.noreply.github.com>
2026-06-15 18:34:08 +0200

    refactor: update base models and add EmailStr validation

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, Field
-
+from pydantic import BaseModel, Field, EmailStr
+from datetime import date
 
 class CarBase(BaseModel):
     brand: str = Field(min_length=1)
@@ -7,19 +7,33 @@ class CarBase(BaseModel):
     year: int = Field(ge=1900, le=2100)
     available: bool
 
-
 class CarCreate(CarBase):
-    id: int
-
+    pass
 
 class CarResponse(CarBase):
     id: int
+    
+    model_config = {"from_attributes": True}
+
 
 class UserCreate(BaseModel):
     name: str = Field(min_length=1)
-    email: str = Field(min_length=1)
-    password: str = Field(min_length=1)
+    email: EmailStr
+    password: str = Field(min_length=6)
 
 class UserLogin(BaseModel):
-    email: str = Field(min_length=1)
+    email: EmailStr
     password: str = Field(min_length=1)
+
+
+class RentalCreate(BaseModel):
+    car_id: int
+
+class RentalResponse(BaseModel):
+    id: int
+    car_id: int
+    user_id: int
+    start_date: date
+    end_date: date
+
+    model_config = {"from_attributes": True}
